5a6dd826df1e0e46176368bc2bda99c2c8cb9fe5
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-10-07 01:13:57 +0600

    Version 1.2.4

--- src/pages/MenuPage/MenuPage.tsx
+++ src/pages/MenuPage/MenuPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import Footer from "../../components/Footer/Footer";
 import Hamburger from "hamburger-react";
 
 import "./styles/menu_page.css";
@@ -19,7 +19,7 @@ const MenuPage = () => {
   };
 
   return (
-    <div className="menu">
+    <menu className="menu">
       <div className="menu-header-container">
         <header className="menu-header">
           <div className="burger">
@@ -39,15 +39,166 @@ const MenuPage = () => {
           <div className="tab-heading">{tab}</div>
         </div>
         <main className="main-menu-content">
-          <div className="menu-row"></div>
-          <div className="menu-row next"></div>
+          <div className="menu-row">
+            <section className="menu-section">
+              <strong className="menu-section-heading">SHOE & BAG</strong>
+              <nav className="links">
+                <a href="/" className="link">
+                  Casual Shoes
+                </a>
+                <a href="/" className="link">
+                  Boots
+                </a>
+                <a href="/" className="link">
+                  Sandals
+                </a>
+                <a href="/" className="link">
+                  Slippers
+                </a>
+              </nav>
+            </section>
+            <section className="menu-section">
+              <strong className="menu-section-heading">HOME TEXTILE</strong>
+              <nav className="links">
+                <a href="/" className="link">
+                  Bedding
+                </a>
+                <a href="/" className="link">
+                  Pillows
+                </a>
+                <a href="/" className="link">
+                  Handkerchief Towels
+                </a>
+                <a href="/" className="link">
+                  Curtain
+                </a>
+              </nav>
+            </section>
+          </div>
+          <div className="menu-row next">
+            {" "}
+            <section className="menu-section">
+              <strong className="menu-section-heading">
+                LUXURY &<br /> DESIGNER
+              </strong>
+              <nav className="links">
+                <a href="/" className="link">
+                  Towels
+                </a>
+                <a href="/" className="link">
+                  Bathroom Scales
+                </a>
+                <a href="/" className="link">
+                  Bath Mats
+                </a>
+                <a href="/" className="link">
+                  Shower Caps
+                </a>
+              </nav>
+            </section>
+            <section className="menu-section">
+              <strong className="menu-section-heading">COSMETICS</strong>
+              <nav className="links">
+                <a href="/" className="link">
+                  Shampoo and Conditioner
+                </a>
+                <a href="/" className="link">
+                  Styling Products
+                </a>
+                <a href="/" className="link">
+                  Hair Accessories
+                </a>
+                <a href="/" className="link">
+                  Men's Grooming
+                </a>
+              </nav>
+            </section>
+          </div>
           <div className="menu-columns">
-            <div className="column first"></div>
-            <div className="column second"></div>
+            <div className="column first">
+              <section className="menu-section">
+                <strong className="menu-section-heading">PARTY SUPPLIES</strong>
+                <nav className="links">
+                  <a href="/" className="link">
+                    Event & Party
+                  </a>
+                  <a href="/" className="link">
+                    Christmas
+                  </a>
+                  <a href="/" className="link">
+                    Artificial Decorations
+                  </a>
+                  <a href="/" className="link">
+                    Wedding
+                  </a>
+                </nav>
+              </section>
+              <section className="menu-section under">
+                <strong className="menu-section-heading">
+                  SPORT & <br /> OUTDOORS
+                </strong>
+                <nav className="links">
+                  <a href="/" className="link">
+                    Team Sports
+                  </a>
+                  <a href="/" className="link">
+                    Water Sports
+                  </a>
+                  <a href="/" className="link">
+                    Outdoor Recreation
+                  </a>
+                  <a href="/" className="link">
+                    Fitness Equipment
+                  </a>
+                </nav>
+              </section>
+            </div>
+            <div className="column second">
+              {" "}
+              <section className="menu-section">
+                <strong className="menu-section-heading">CLOTHES</strong>
+                <nav className="links">
+                  <a href="/" className="link">
+                    Bottoms
+                  </a>
+                  <a href="/" className="link">
+                    Women's Clothing{" "}
+                  </a>
+                  <a href="/" className="link">
+                    T-Shirts and Tops
+                  </a>
+                  <a href="/" className="link">
+                    Dresses
+                  </a>
+                  <a href="/" className="link">
+                    Outerwear
+                  </a>
+                  <a href="/" className="link">
+                    Formal Wear
+                  </a>
+                  <a href="/" className="link">
+                    Casual Wear
+                  </a>
+                  <a href="/" className="link">
+                    Seasonal Collections
+                  </a>
+                  <a href="/" className="link">
+                    Sports Bras
+                  </a>
+                  <a href="/" className="link">
+                    Workout Tops
+                  </a>
+                  <a href="/" className="link">
+                    Fall Wardrobe
+                  </a>
+                </nav>
+              </section>
+            </div>
           </div>
         </main>
       </div>
-    </div>
+      <Footer />
+    </menu>
   );
 };
 
